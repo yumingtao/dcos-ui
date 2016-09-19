de1364da08d396e22f5f3f7790fe581b2b249966
Dmitrii Rozhkov <drozhkov@mesosphere.io>
2016-09-19 15:54:37 +0200

    render pod labels

--- src/js/components/PodSpecView.js
+++ src/js/components/PodSpecView.js
@@ -55,6 +55,24 @@ class PodSpecView extends React.Component {
     );
   }
 
+  getLabelsDetails() {
+    let {spec} = this.props;
+    let labels = spec.getLabels();
+
+    if (Object.keys(labels).length === 0) {
+      return null;
+    }
+
+    return (
+      <div>
+        <h4 className="inverse flush-top">
+          Labels
+        </h4>
+        <DescriptionList hash={labels} />
+      </div>
+    );
+  }
+
   getGeneralDetails() {
     let {spec} = this.props;
     let hash = {
@@ -73,6 +91,7 @@ class PodSpecView extends React.Component {
           General
         </h4>
         {this.getGeneralDetails()}
+        {this.getLabelsDetails()}
         {this.getEnvironmentDetails()}
         <h4 className="inverse flush-top">
           Containers
