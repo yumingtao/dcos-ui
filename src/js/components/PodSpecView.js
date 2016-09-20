b9c63af6abd1d8a2a882b6329658e3db5edfa576
Dmitrii Rozhkov <drozhkov@mesosphere.io>
2016-09-20 17:30:23 +0200

    render pod environment
    env tests

--- src/js/components/PodSpecView.js
+++ src/js/components/PodSpecView.js
@@ -3,6 +3,7 @@ import React from 'react';
 import DescriptionList from './DescriptionList';
 import PodSpec from '../structs/PodSpec';
 import PodContainerSpecView from './PodContainerSpecView';
+import Icon from './Icon';
 
 const METHODS_TO_BIND = [
 ];
@@ -17,6 +18,41 @@ class PodSpecView extends React.Component {
     METHODS_TO_BIND.forEach((method) => {
       this[method] = this[method].bind(this);
     });
+  }
+
+  getEnvironmentDetails() {
+    let {spec} = this.props;
+    let environment = Object.assign({}, spec.getEnvironment());
+
+    if (Object.keys(environment).length === 0) {
+      return null;
+    }
+
+    Object.keys(environment).forEach(function (key) {
+      let value = environment[key];
+      if (typeof value === 'object' && !Array.isArray(value) &&
+          value !== null) {
+        environment[key] = (
+          <span>
+            <Icon
+              className="icon-margin-right"
+              color="white"
+              family="mini"
+              id="key"
+              size="mini" /> {value.secret}
+          </span>
+        );
+      }
+    });
+
+    return (
+      <div>
+        <h4 className="inverse flush-top">
+          Environment variables
+        </h4>
+        <DescriptionList hash={environment} />
+      </div>
+    );
   }
 
   getGeneralDetails() {
@@ -36,7 +72,8 @@ class PodSpecView extends React.Component {
         <h4 className="inverse flush-top">
           General
         </h4>
-        {this.getGeneralDetails(spec)}
+        {this.getGeneralDetails()}
+        {this.getEnvironmentDetails()}
         <h4 className="inverse flush-top">
           Containers
         </h4>
