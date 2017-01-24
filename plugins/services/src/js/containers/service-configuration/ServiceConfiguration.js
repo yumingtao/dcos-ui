da0d2ebb30938e152284d3164f6206bc6eb6fa49
mlunoe <michael.lunoe@gmail.com>
2017-01-24 11:50:32 -0800

    Fixing edit actions for service around the application

--- plugins/services/src/js/containers/service-configuration/ServiceConfiguration.js
+++ plugins/services/src/js/containers/service-configuration/ServiceConfiguration.js
@@ -2,6 +2,7 @@ import {DCOSStore} from 'foundation-ui';
 import {Dropdown} from 'reactjs-components';
 import mixin from 'reactjs-mixin';
 import React from 'react';
+import {routerShape} from 'react-router';
 import {StoreMixin} from 'mesosphere-shared-reactjs';
 
 import ApplicationSpec from '../../structs/ApplicationSpec';
@@ -57,12 +58,12 @@ class ServiceConfiguration extends mixin(StoreMixin) {
   }
 
   handleApplyButtonClick() {
-    const {editService, service} = this.props;
+    const {onEditClick, service} = this.props;
     const {selectedVersionID} = this.state;
 
     const serviceConfiguration = service.getVersions().get(selectedVersionID);
 
-    editService(service,
+    onEditClick(service,
       ServiceUtil.getDefinitionFromSpec(
           new ApplicationSpec(serviceConfiguration)
       )
@@ -75,7 +76,10 @@ class ServiceConfiguration extends mixin(StoreMixin) {
 
     const service = ServiceUtil.createServiceFromResponse(serviceConfiguration);
 
-    this.context.modalHandlers.editService({service});
+    this.context.router.push(
+      `/services/overview/${encodeURIComponent(service.getId())}/edit`
+    );
+
   }
 
   handleVersionSelection(versionItem) {
@@ -190,7 +194,9 @@ class ServiceConfiguration extends mixin(StoreMixin) {
     if (config == null) {
       content = <Loader />;
     } else {
-      content = <ServiceConfigDisplay appConfig={config} />;
+      content = (
+        <ServiceConfigDisplay appConfig={config} />
+      );
     }
 
     return (
@@ -204,13 +210,11 @@ class ServiceConfiguration extends mixin(StoreMixin) {
 }
 
 ServiceConfiguration.contextTypes = {
-  modalHandlers: React.PropTypes.shape({
-    editService: React.PropTypes.func.isRequired
-  }).isRequired
+  router: routerShape
 };
 
 ServiceConfiguration.propTypes = {
-  editService: React.PropTypes.func.isRequired,
+  onEditClick: React.PropTypes.func.isRequired,
   service: React.PropTypes.instanceOf(Service).isRequired
 };
 
