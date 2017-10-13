dfec6454576582e0e9e5ed566d9f4b188635dd4c
Andrew Hoskins <ahoskins@mesosphere.com>
2017-10-13 10:17:17 -0700

    feat(FrameworkConfiguration): avoid bad css override of fieldset and legend

--- src/js/components/FrameworkConfigurationForm.js
+++ src/js/components/FrameworkConfigurationForm.js
@@ -236,7 +236,6 @@ export default class FrameworkConfigurationForm extends Component {
       deployErrors
     } = this.props;
 
-    // nicely format titles rendered by the json-schema library
     const TitleField = props => {
       return (
         <h2 className="flush-top short-bottom">
