bf88882606c6054a394b10fa21abc57bb19492b3
Andrew Hoskins <ahoskins@mesosphere.com>
2017-10-13 09:32:19 -0700

    feat(FrameworkConfiguration): fix scrolling of form/json and better duplicate package error

--- src/js/components/FrameworkConfigurationForm.js
+++ src/js/components/FrameworkConfigurationForm.js
@@ -5,6 +5,7 @@ import TabButton from "#SRC/js/components/TabButton";
 import TabButtonList from "#SRC/js/components/TabButtonList";
 import Tabs from "#SRC/js/components/Tabs";
 import Util from "#SRC/js/utils/Util";
+import FluidGeminiScrollbar from "#SRC/js/components/FluidGeminiScrollbar";
 import JSONEditor from "#SRC/js/components/JSONEditor";
 import PageHeaderNavigationDropdown
   from "#SRC/js/components/PageHeaderNavigationDropdown";
@@ -293,7 +294,7 @@ export default class FrameworkConfigurationForm extends Component {
                 </div>
               </Tabs>
             </div>
-          </div>
+          </FluidGeminiScrollbar>
         </div>
         <div className={jsonEditorPlaceholderClasses} />
         <div className={jsonEditorClasses}>
